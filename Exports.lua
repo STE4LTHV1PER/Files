43d20a6b4eb8636d613759ad0ff2d2cd77e2109c
STE4LTHV1PER <ste4lthstudios@gmail.com>
2026-07-08 16:34:27 -0500

    Refactor Promise type definitions in Exports.lua

--- Exports.lua
+++ Exports.lua
@@ -1,26 +1,45 @@
+export type PromiseOptions = {
+	
+	cancellable: boolean?,
+	
+}
+
 export type PromiseStatus = "Pending" | "Fulfilled" | "Rejected"
 
-export type PromiseOptions = {
-	cancellable: boolean?,
+export type ErrorTypes = "ValidationError" | "RuntimeError" | "CancelledError" | "TimeoutError" | "UnknownError"
+
+export type Error = {
+	
+	type: ErrorTypes,
+	message: string?,
+	trace: string?,
+	context: string?,
+	
 }
 
 export type Executor<T> = (
+	
 	resolve: (T) -> (),
 	reject: (any) -> ()
+	
 ) -> ()
 
 export type PromiseInternal<T> = {
+	
 	Status: PromiseStatus,
 	Value: T?,
 	Error: any,
 	Cancelled: boolean,
 	SuccessCallbacks: { (T) -> () },
 	ErrorCallbacks: { (any) -> () },
+	
 }
 
 export type Promise<T> = {
+	
 	andThen: <U>(self: Promise<T>, callback: (T) -> U) -> Promise<U>,
-	catch: <U>(self: Promise<T>, callback: (any) -> U) -> Promise<T | U>,
+	catch: (self: Promise<T>, callback: (Error) -> ()) -> Promise<T>,
+	
 }
 
 return {}
